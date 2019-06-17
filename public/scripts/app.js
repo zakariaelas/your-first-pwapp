94027a3214439a85ce4c3f626502eb62888320f7
zakariasaad <zakariasaad@vt.edu>
2019-06-17 21:33:31 +0100

    enhanced offline experience

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -163,6 +163,19 @@ function getForecastFromNetwork(coords) {
  */
 function getForecastFromCache(coords) {
   // CODELAB: Add code to get weather forecast from the caches object.
+  if(!('caches' in window)) {
+    return null;
+  }
+  const url = `${window.location.origin}/forecast/${coords}`;
+  return caches.match(url)
+    .then(res => {
+      if (res) return res.json();
+      return null;
+    })
+    .catch(err => {
+      console.error('Error getting data from cache', err);
+      return null;
+    })
 
 }
 
@@ -198,7 +211,10 @@ function updateData() {
     const location = weatherApp.selectedLocations[key];
     const card = getForecastCard(location);
     // CODELAB: Add code to call getForecastFromCache
-
+    getForecastFromCache(location.geo)
+      .then(forecast => {
+        renderForecast(card, forecast);
+      });
     // Get the forecast data from the network.
     getForecastFromNetwork(location.geo)
         .then((forecast) => {
